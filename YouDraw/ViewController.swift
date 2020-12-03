17899d08696f843489ef8bfaddd32f92fb8e8084
S10House <GS_FS@Live.com>
2020-12-03 11:40:25 +0500

    Initial Commit

---
+++ YouDraw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouDraw
+//
+//  Created by GSK on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
